be5167a6e8cc310b766748b7b1956f2811b558b4
kosta kostyukov <kostakostyukov@gmail.com>
2022-06-19 20:14:01 +0300

    created the base project for the client

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# colman-bookshop-client
+This is the client side of our web apps course's final project.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p>this is a test</p>
+    </div>
+  );
+}
+
+export default App;
